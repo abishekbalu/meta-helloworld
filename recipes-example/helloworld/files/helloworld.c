429c8e07c8e6c5e8d6afba6c2ba68311e6b39030
abishekbalu <abishekbalu@outlook.com>
2026-09-06 22:55:22 +0200

    Create a new meta-helloworld layer for yocto project

---
+++ recipes-example/helloworld/files/helloworld.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("\n============================\n");
+    printf("    Hello World from Custom Yocto Layer!!   \n");
+    printf("\n============================\n");
+    return 0;
+}
